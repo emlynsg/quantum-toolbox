49bdb7ffc4569c531b8a5d9ebbb820aaca50620c
EmlynG <emlyn.graham@gmail.com>
2019-08-21 10:33:26 +1000

    Broken example set in main

--- main.cpp
+++ main.cpp
@@ -15,27 +15,60 @@
 #include "System.h"
 #include "Plotter.h"
 
-int main() {
+std::array<double, 1024> transformArray(std::array<double, 1024> &array){
+  std::array<double, 1024> ret;
+  std::transform(array.begin(), array.end(), ret.begin(), [](auto &elt) { return 2.5*elt; });
+}
 
-  int sizeN = 4095;
+void alterArray(std::array<double, 1024> &array){
+  std::transform(array.begin(), array.end(), array.begin(), [](auto &elt) { return 2.5*elt; });
+}
+
+std::vector<double> transformVector(std::vector<double> &vec){
+  std::vector<double> ret(1024);
+  std::transform(vec.begin(), vec.end(), ret.begin(), [](auto &elt) { return 2.5*elt; });
+}
+
+void alterVector(std::vector<double> &vec){
+  std::transform(vec.begin(), vec.end(), vec.begin(), [](auto &elt) { return 2.5*elt; });
+}
+
+void test(){
+  for (int k = 0; k < 100; ++k) {
+    std::vector<double> testvec(1024);
+    std::array<double, 1024> testarray ;
+    for (int j = 0; j < 1024; ++j) {
+      testvec[j] = j;
+      testarray[j] = j;
+    }
+    testarray = transformArray(testarray);
+    testvec = transformVector(testvec);
+    alterArray(testarray);
+    alterVector(testvec);
+  }
+
+}
+
+int main() {
+  /// Consider using C++17 for the parallel operations
+  /// https://www.bfilipek.com/2018/11/parallel-alg-perf.html
+
+
+  /// Error example: Seems to go wrong when changing psiPart inside the Taylor expansion loop in evolve from System.cpp
+  int sizeN = 1023;
   double xmin = -200.0;
   double xmax = 200.0;
   double kscale = 1.0;
-
   Grid gridObject(sizeN, xmin, xmax, kscale);
-
   double ReducedMass = 1;
   Wavefunction waveObject(gridObject, ReducedMass);
-  waveObject.initGaussian(0.0, 10.0);
-
+  waveObject.initConstant();
   Potential pot(gridObject);
   pot.initZero();
-
+  pot.addParabolic(0.0, 20.0);
   System sys(waveObject, pot);
-
-
   Plotter plot(sys);
-  plot.animate(30000, 0.1, 20);
+  plot.animate(10, 0.1, 20);
 
 
   /*
@@ -61,3 +94,4 @@ int main() {
   return 0;
 
 }
+
